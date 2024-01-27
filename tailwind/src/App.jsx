43a8f4c1ad654a65df309e264e001056d33f55f7
Ayush9535 <ghodkeayush22@gmail.com>
2024-01-27 11:50:01 +0530

    Tailwind used

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Tailwind from './Components/Tailwind'
+
+function App() {
+  return (
+    <>
+      <Tailwind />
+    </>
+  )
+}
+
+export default App
